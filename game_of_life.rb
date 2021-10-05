e911be452bbbf4e117f856ada7f3d20b548f2a49
Patrick Barsch <PatrickGB@gmx.de>
2021-10-05 15:59:31 +0200

    Refactored the function to calculate the next generation

--- game_of_life.rb
+++ game_of_life.rb
@@ -1,6 +1,5 @@
 # Game of life
 
-# Input:
 def get_user_input
   puts "How big do you want your grid (square)?"
   grid_size = gets.chomp.to_i
@@ -10,8 +9,8 @@ def get_user_input
 end
 # - load a population from a file
 
-# let's mock this for now
-
+# Let's use and helping edge that is always 0, to avoid having to adjust
+# the neighbour_calculation function
 def setup_grid_with_edge(grid_size)
   input = Array.new(grid_size) { Array.new(grid_size,rand(2)) }
   grid = Array.new(grid_size + 2) { Array.new(grid_size + 2, 0) }
@@ -21,29 +20,35 @@ def setup_grid_with_edge(grid_size)
   grid
 end
 
-# Run the game:
-# - calculate the next generation for the defined number of iterations
 def calculate_next_generation(grid, grid_size, position)
-  # calculate the value for all the neighbours
+  # Determine where we are in the grid.
   row = (position / grid_size) + 1
   column = (position % grid_size) + 1
-  cell = grid[row][column]
+  number_of_neighbours = calculate_neighbours(grid, row, column)
+  # Move further, if we did not reach the end of the grid.
+  unless position == 0
+    calculate_next_generation(grid, grid_size, position-1)
+  end
+  # Update the cell
+  grid[row][column] = dead_or_alive(grid[row][column], number_of_neighbours)
+  return grid
+end
+
+def calculate_neighbours(grid, row, column)
   neighbours = grid[row-1..row+1].map do |neighbour_row|
     neighbour_row[column-1..column+1].sum
   end
-  number_of_neighbours = neighbours.sum - cell
-  # check if we reached the end of the grid
-  unless position == 0
-      # --> if not, call this function recursively
-    calculate_next_generation(grid, grid_size, position-1)
+  number_of_neighbours = neighbours.sum - grid[row][column]
+end
+
+def dead_or_alive(cell, neighbours)
+  if neighbours == 3
+    return 1
+  elsif neighbours != 2
+    return 0
+  else
+    return cell 
   end
-  # switch the cell based on its own and its neighbours status
-  if number_of_neighbours == 3
-    grid[row][column] = 1
-  elsif number_of_neighbours != 2
-    grid[row][column] = 0 
-  end
-  return grid
 end
 
 def print_grid (grid, grid_size)
@@ -62,13 +67,14 @@ def print_game_over(how_long)
   puts "Better luck next time."
 end
 
+# Run the game:
 grid_size, generations = get_user_input
 end_position = grid_size**2 - 1
 grid = setup_grid_with_edge(grid_size)
 generations.times do |time|
   grid = calculate_next_generation(grid, grid_size, end_position)
   if grid.map{|row| row.sum}.sum == 0
-    print_game_over(times)
+    print_game_over(time)
     break
   end
   print_grid(grid, grid_size)
