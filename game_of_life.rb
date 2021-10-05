49188031cad4b9ac932e4fc733cc47b30e1d84d0
Patrick Barsch <PatrickGB@gmx.de>
2021-10-05 11:35:56 +0200

    Calculating the number of neighbours for the first cell

--- game_of_life.rb
+++ game_of_life.rb
@@ -19,16 +19,24 @@ def setup_grid_with_edge(grid_size, input)
 end
 
 grid = setup_grid_with_edge(grid_size, input)
-print grid
 
 # Run the game:
 # - calculate the next generation for the defined number of iterations
 def calculate_next_generation(grid, grid_size, generation)
   # calculate the value for all the neighbours
+  row = (generation / grid_size) + 1
+  column = (generation % grid_size) + 1
+  neighbours = grid[row-1..row+1].map do |neighbour_row|
+    neighbour_row[column-1..column+1].sum
+  end
+  number_of_neighbours = neighbours.sum - grid[row][column]
+  puts number_of_neighbours
   # check if we reached the end of the grid
   # --> if not, call this function recursively
   # switch the cell based on its own and its neighbours status
 end
+
+calculate_next_generation(grid, grid_size, 0)
 
 # Output:
 # - print the grid to stdout
