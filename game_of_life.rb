89f5aefb99521ff7849c7878fbe5a5e4115f260f
Patrick Barsch <PatrickGB@gmx.de>
2021-10-05 13:19:12 +0200

    First succesful iteration

--- game_of_life.rb
+++ game_of_life.rb
@@ -21,24 +21,31 @@ def calculate_next_generation(grid, grid_size, generation)
   # calculate the value for all the neighbours
   row = (generation / grid_size) + 1
   column = (generation % grid_size) + 1
+  cell = grid[row][column]
   neighbours = grid[row-1..row+1].map do |neighbour_row|
     neighbour_row[column-1..column+1].sum
   end
-  number_of_neighbours = neighbours.sum - grid[row][column]
+  number_of_neighbours = neighbours.sum - cell
   # check if we reached the end of the grid
   unless generation == grid_size**2 - 1
       # --> if not, call this function recursively
     calculate_next_generation(grid, grid_size, generation+1)
   end
-  puts number_of_neighbours
   # switch the cell based on its own and its neighbours status
+  if number_of_neighbours == 3
+    grid[row][column] = 1
+  elsif number_of_neighbours != 2
+    grid[row][column] = 0 
+  end
+  return grid
 end
 
 grid_size = 3
 generation = 1
 input = [[0,1,0], [1,1,1], [0,1,0]]
 grid = setup_grid_with_edge(grid_size, input)
-calculate_next_generation(grid, grid_size, 0)
+grid = calculate_next_generation(grid, grid_size, 0)
+print grid
 
 # Output:
 # - print the grid to stdout
