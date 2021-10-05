adde0da4ee99853946f572150c91ee33f1445e90
Patrick Barsch <PatrickGB@gmx.de>
2021-10-05 09:18:23 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# A "Game of life" implementation in Ruby
+
+Just another implementation of Conways famous game of life.
+I try to implement some thing I learned about traversing 2D-Data structures and recursion.
+
+## How to run it:
+``` shell
+ruby game_of_life.rb
+´´´
---
+++ game_of_life.rb
@@ -0,0 +1,13 @@
+# Game of life
+
+# Input:
+# - get some input from the user through the terminal (grid size, number of iterations, time between iterations)
+# - generate a random population
+# - load a population from a file
+
+# Run the game:
+# - calculate the next generation for the defined number of iterations
+
+# Output:
+# - print the grid to stdout
+# - wait for the pre-defined time
